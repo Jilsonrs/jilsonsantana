ae533195495fe90fb21d25256a50cb795314a9c9
Jilsonrs <jilsonsantana@yahoo.com>
2025-12-06 17:49:23 +0000

    Update settings.py - clean production config

--- website/settings.py
+++ website/settings.py
@@ -85,18 +85,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "website.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
@@ -131,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 
